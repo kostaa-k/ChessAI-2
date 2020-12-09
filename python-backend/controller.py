db72a91f375b2731692345fc74faff8ad28356da
Kosta <thekostakosta@gmail.com>
2020-12-08 19:51:01 -0500

    updating controller for reset games

--- python-backend/controller.py
+++ python-backend/controller.py
@@ -1,6 +1,8 @@
 from flask import Flask, request
 from flask_cors import CORS, cross_origin
 
+from copy import deepcopy
+
 import json
 import pandas
 import chess
@@ -17,6 +19,7 @@ import chessFunctions
 app = Flask(__name__)
 cors = CORS(app)
 
+numOfGames = 0
 
 def getStringArray(aBoard):
     boardArray = []
@@ -35,6 +38,16 @@ def example_func():
 
     materialEvalStr = (request.args.get("MaterialEval"))
     movesEvalStr = (request.args.get("MovesEval"))
+
+    global numOfGames
+    originalGameNum = deepcopy(numOfGames)
+    firstBoard = False
+    
+    #gameNum = (int)(request.args.get("GameNum"))
+
+    if(theFen == "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"):
+        numOfGames = numOfGames+1
+        firstBoard = True
 
     #print("material eval stR: ", materialEvalStr, "movesEval str: ", movesEvalStr)
 
@@ -79,7 +92,8 @@ def example_func():
     print()
     print()
 
-    return returnDict
+    if(firstBoard == True or originalGameNum==numOfGames):
+        return returnDict
 
 
 
